42c8cff2279ee1dc069d5aa30709c3f007b787d6
James Radtke - SUSE <james.radtke@suse.com>
2025-10-13 13:57:49 -0400

    Making output more searchable

--- observability/outputs.tf
+++ observability/outputs.tf
@@ -66,7 +66,7 @@ output "kubectl_commands" {
     ssh -i ~/.ssh/suse-demo-aws.pem ec2-user@${var.create_eip ? aws_eip.observability[0].public_ip : aws_instance.observability.public_ip} "sudo kubectl logs -n suse-observability -l app=suse-observability-router --tail=100"
 
     # Get Default Admin password 
-    ssh -i ~/.ssh/suse-demo-aws.pem ec2-user@${var.create_eip ? aws_eip.observability[0].public_ip : aws_instance.observability.public_ip} "grep pass /var/log/user-data.log"
+    ssh -i ~/.ssh/suse-demo-aws.pem ec2-user@${var.create_eip ? aws_eip.observability[0].public_ip : aws_instance.observability.public_ip} "grep passw /var/log/user-data.log"
   EOT
 }
 
